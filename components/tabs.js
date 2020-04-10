34d1b741ef7a8f6fe19dbf2cd34e5c8a99d9f7f0
Tamar <mischegoss@gmail.com>
2020-04-10 19:01:51 -0400

    Fix tab

--- components/tabs.js
+++ components/tabs.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Header, Tab, Tabs, TabHeading, Icon, Text } from 'native-base';
-
+import AccordianMain from './accordian'
 export default class MyTabs extends Component {
   render() {
     return (
@@ -8,7 +8,7 @@ export default class MyTabs extends Component {
         <Header hasTabs/>
         <Tabs>
           <Tab heading={ <TabHeading><Icon name="camera" /><Text>All Decks</Text></TabHeading>}>
-            <Text>This1</Text>
+            <AccordianMain/>
           </Tab>
           <Tab heading={ <TabHeading><Text>Add New</Text></TabHeading>}>
           <Text>This2</Text>
